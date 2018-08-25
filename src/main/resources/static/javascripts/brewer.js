f2d10df1319277e80328611178dec729b653679d
João Victor <joaovictor.cogomes@gmail.com>
2018-08-25 09:16:04 -0300

    Novo plugin de moeda

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -8,8 +8,10 @@ Brewer.MaskMoney = (function() {
 	}
 	
 	MaskMoney.prototype.enable = function() {
-		this.decimal.maskMoney({ decimal: ',', thousands: '.'});
-		this.plain.maskMoney({ precision: 0, thousands: '.', decimal: '' });
+		//this.decimal.maskMoney({ decimal: ',', thousands: '.'});
+		//this.plain.maskMoney({ precision: 0, thousands: '.', decimal: '' });
+		this.decimal.maskNumber({ decimal: ',', thousands: '.'});
+		this.plain.maskNumber({ integer: true, thousands: '.' });
 	}
 	
 	return MaskMoney;
